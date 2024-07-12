24dfdd4d9920f5fed9813851068fea12b4545916
CarlosNes <carlosnes16@gmail.com>
2024-07-12 09:39:56 -0600

    Nueva pantalla de inicio

--- app/src/main/java/com/example/ejercicioactividad/PantalladeInicio.kt
+++ app/src/main/java/com/example/ejercicioactividad/PantalladeInicio.kt
@@ -1,30 +1,20 @@
 package com.example.ejercicioactividad
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class PantalladeInicio : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_pantallade_inicio)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-    val btningresar: Button=findViewById(R.id.btningresar)
-        btningresar.setOnClickListener {
-            val intent=Intent (this,PantalladeInicio::class.java)
-            startActivity(intent)
-            Toast.makeText(this, "Bienvendido", Toast.LENGTH_SHORT).show()
         }
     }
 }
